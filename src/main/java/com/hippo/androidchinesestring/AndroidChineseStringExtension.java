19602e90a4b900e3c73034e895db0e4306b44795
seven332 <seven332@163.com>
2017-02-09 19:33:16 +0800

    Init

---
+++ src/main/java/com/hippo/androidchinesestring/AndroidChineseStringExtension.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017 Hippo Seven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hippo.androidchinesestring;
+
+/*
+ * Created by Hippo on 2/9/2017.
+ */
+
+public class AndroidChineseStringExtension {
+
+  public static final String CONFIG_CN_HK = "s2hk.json";
+  public static final String CONFIG_CN_TW = "s2twp.json";
+
+  private String url = "http://opencc.byvoid.com/convert";
+  private String textName = "text";
+  private String configName = "config";
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public String getTextName() {
+    return textName;
+  }
+
+  public void setTextName(String textName) {
+    this.textName = textName;
+  }
+
+  public String getConfigName() {
+    return configName;
+  }
+
+  public void setConfigName(String configName) {
+    this.configName = configName;
+  }
+}
---
+++ src/main/java/com/hippo/androidchinesestring/AndroidChineseStringTask.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2017 Hippo Seven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hippo.androidchinesestring;
+
+/*
+ * Created by Hippo on 2/9/2017.
+ */
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+
+public class AndroidChineseStringTask extends DefaultTask {
+
+  @TaskAction
+  public void convert() {
+    AndroidChineseStringExtension extension =
+        getProject().getExtensions().findByType(AndroidChineseStringExtension.class);
+    if (extension == null) {
+      extension = new AndroidChineseStringExtension();
+    }
+
+    // TODO
+    System.out.println(getProject().getProjectDir());
+    System.out.println(extension.getUrl());
+  }
+}
